46354ea3f4231465486d68a7690736ae9cafd7a3
ramazan_aisaev <aisaev-68@yandex.ru>
2023-10-13 23:37:46 +0300

    Исправлены все недоробтки

--- tests/test_db.py
+++ tests/test_db.py
@@ -10,16 +10,16 @@ from app.models.models import Answer
 @pytest.mark.asyncio
 async def test_add_get_data(get_db: AsyncSession):
     add_data = Answer(
-        question_id=12345,
-        question='Test question1',
-        answer='Test answer1',
+        question_id=14499,
+        question='Test question55',
+        answer='Test answer55',
         created_at=datetime.datetime(2023, 12, 30, 12, 34, 56)
     )
     get_db.add(add_data)
     await get_db.flush()
     await get_db.commit()
 
-    stmt = select(Answer).where(Answer.question == "Test question1")
+    stmt = select(Answer).where(Answer.question == "Test question55")
     result = await get_db.execute(stmt)
     quest = result.scalars().first()
     assert quest.id == add_data.id
