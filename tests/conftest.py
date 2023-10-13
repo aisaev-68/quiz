4c5b17ac2d5d0cd758d260c39c5b9b3afd3c8621
ramazan_aisaev <aisaev-68@yandex.ru>
2023-10-13 20:24:36 +0300

    Исправлены все недоробтки

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,26 @@ import pytest_asyncio
 from httpx import AsyncClient
 
 from app.main import app
+from app.models.database import get_db
 
 
 @pytest_asyncio.fixture(scope="session")
 def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    try:
+        yield loop
+    finally:
+        loop.close()
 
 
 @pytest_asyncio.fixture(scope="session")
-async def client():
-    async with AsyncClient(app=app, base_url="http://0.0.0.0:8000") as async_client:
-        yield async_client
+async def async_db():
+    async with get_db() as db:
+        yield db
+
+
+@pytest_asyncio.fixture(scope="session")
+async def async_client():
+    async with AsyncClient(app=app, base_url="http://0.0.0.0:8000") as client:
+        yield client
